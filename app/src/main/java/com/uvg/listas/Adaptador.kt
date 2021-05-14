eef758bb9cb8bc035c169e73d8bf32a32a512e9f
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-14 15:02:50 -0600

    cambios

--- app/src/main/java/com/uvg/listas/Adaptador.kt
+++ app/src/main/java/com/uvg/listas/Adaptador.kt
@@ -6,9 +6,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_lista.view.*
 
-class Adaptador(var items: MutableList<String>): RecyclerView.Adapter<Adaptador.ItemsViewHolder>() {
+class Adaptador(/*var items: MutableList<String>*/): RecyclerView.Adapter<Adaptador.ItemsViewHolder>() {
 
-    //private var items: MutableList<String> = mutableListOf()
+    private var items: MutableList<String> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista,parent,false)
@@ -22,6 +22,11 @@ class Adaptador(var items: MutableList<String>): RecyclerView.Adapter<Adaptador.
 
     override fun getItemCount(): Int {
         return items.size
+    }
+
+    fun setItems(nuevosItems: MutableList<String>){
+        this.items = nuevosItems
+        notifyDataSetChanged()
     }
 
     class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/uvg/listas/MainActivity.kt
+++ app/src/main/java/com/uvg/listas/MainActivity.kt
@@ -7,18 +7,56 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //private val adaptador = Adaptador()
+    private val adaptador = Adaptador()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       /* val items: MutableList<String> = mutableListOf()
+        items.add("Prueba 1")
+        items.add("Prueba 2")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")
+        items.add("Prueba 3")*/
+
+        //val adaptador = Adaptador(adaptador)
+
         val items: MutableList<String> = mutableListOf()
+
         items.add("Prueba 1")
         items.add("Prueba 2")
         items.add("Prueba 3")
 
-        val adaptador = Adaptador(items)
+
+        adaptador.setItems(items)
 
         lista.layoutManager = LinearLayoutManager(this)
         lista.adapter = adaptador
