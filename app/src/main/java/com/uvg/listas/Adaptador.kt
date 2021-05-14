3520c57b0d2819805fbdfda1ff80acf51de49227
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-14 14:27:08 -0600

    cambios

--- app/src/main/java/com/uvg/listas/Adaptador.kt
+++ app/src/main/java/com/uvg/listas/Adaptador.kt
@@ -1,19 +1,21 @@
 package com.uvg.listas
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class Adaptador: RecyclerView.Adapter<Adaptador.ItemsViewMolder>() {
+class Adaptador: RecyclerView.Adapter<Adaptador.ItemsViewHolder>() {
 
     //Creando lista.
     private var items: MutableList<String> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewMolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista,parent,false)
+        return ItemsViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ItemsViewMolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
@@ -21,5 +23,5 @@ class Adaptador: RecyclerView.Adapter<Adaptador.ItemsViewMolder>() {
         return items.size //Retornando el tamaño de la lista.
     }
 
-    class ItemsViewMolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
